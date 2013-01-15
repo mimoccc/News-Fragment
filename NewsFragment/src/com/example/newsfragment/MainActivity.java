e91b30e4e42e7116f6a91c9ea15c4379e8b27dcd
Rockets <rockets111@rockets.com>
2013-01-15 12:15:32 +0800

    Initial Commit

---
+++ NewsFragment/src/com/example/newsfragment/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.newsfragment;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+
+public class MainActivity extends FragmentActivity {
+	
+	private static String TAG = "MainActivity";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i(TAG, "onCreate");
+        
+        setContentView(R.layout.fragment_layout_support);
+    }   
+}
